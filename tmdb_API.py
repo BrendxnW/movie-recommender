e2af7e520e9885c19f3361f6d2e5f51bc05d16e5
BrendxnW <BrendonHorse18@gmail.com>
2025-06-15 16:17:19 -0700

    get movie id and work on get movie by genere

--- tmdb_API.py
+++ tmdb_API.py
@@ -5,3 +5,37 @@ import requests
 load_dotenv()
 API_KEY = os.getenv("TMDB_API_KEY")
 BASE_URL = "https://api.themoviedb.org/3"
+BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMDhiN2ZmNTIzMWMyNTE3YzQ3YjEyZmM5NTk3YTFiYyIsIm5iZiI6MTc0ODU2NzMyMy42MzUsInN1YiI6IjY4MzkwNTFiYzdkMWEzYjgyOTA4MDdlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BeX-zrb02dndbiIdfly0cNqeRlA6gtG0rUjvuHhEJC8"
+
+
+def get_movie_genre_id():
+    """
+    Gets the movie genere id
+    """
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer {BEARER_TOKEN}"
+    }
+
+    response = requests.get(url, headers=headers)
+    genres = response.json().get("genres", [])
+    return {genre["names"].lower() : genre["id"] for genre in genres}
+    
+
+def get_movies_by_genere(genre):
+    """
+    Gets movies by genere
+    """
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer {BEARER_TOKEN}"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    return response.text
+
