5a9c7a06abf3cce40d75043a24052a45f3f89416
BrendxnW <BrendonHorse18@gmail.com>
2025-06-15 17:04:17 -0700

    fix get movie genre id

---
+++ recommender.py
@@ -0,0 +1,8 @@
+from nlp_utils import ReccomendMovie
+
+
+class FindMovie:
+    """
+    Finds a couple movie suggestions
+    """
+    pass
--- tmdb_API.py
+++ tmdb_API.py
@@ -2,39 +2,51 @@ import os
 from dotenv import load_dotenv
 import requests
 
+
 load_dotenv()
 API_KEY = os.getenv("TMDB_API_KEY")
 BASE_URL = "https://api.themoviedb.org/3"
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
-def get_movie_genre_id():
+
+class InvalidGenreError(Exception):
     """
-    Gets the movie genere id
+    Custom Error if there is an invalid genre
+    """
+    pass
+
+def get_movie_genre_id(genre_name):
+    """
+    Gets the movie id from the given genre
     """
     url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
 
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer {BEARER_TOKEN}"
+        "Authorization": f"Bearer {BEARER_TOKEN}"
     }
-
     response = requests.get(url, headers=headers)
-    genres = response.json().get("genres", [])
-    return {genre["names"].lower() : genre["id"] for genre in genres}
-    
 
-def get_movies_by_genere(genre):
+    genres = response.json().get("genres", [])
+    for genre in genres:
+        if genre["name"].lower() == genre_name.lower():
+            return genre["id"]
+    raise InvalidGenreError(f"Unfamiliar with the genre: \"{genre_name}\"")
+
+
+def get_movies_by_genre(genre):
     """
-    Gets movies by genere
+    Gets movies by genre
     """
+    genre_id = get_movie_genre_id(genre.lower())
+
     url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
 
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer {BEARER_TOKEN}"
+        "Authorization": f"Bearer {BEARER_TOKEN}"
     }
 
     response = requests.get(url, headers=headers)
 
     return response.text
-
