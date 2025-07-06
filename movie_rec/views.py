ee2cd67f8f2ae38f149eda5c5c44d18898ba6a45
Brendon Wong <BrendonHorse18@gmail.com>
2025-07-05 21:28:42 -0700

    fix front end back button and recommender

--- movie_rec/views.py
+++ movie_rec/views.py
@@ -3,43 +3,58 @@ from .nlp_utils import RecommendMovie, GreetingPrompt
 
 
 def greet_view(request):
+    # Default context
     context = {
         "greeting": None,
         "feature": None,
         "movie_result": None,
     }
 
+    # Get current step from session, default to greeting
+    step = request.session.get('step', 'greeting')
+
     if request.method == "POST":
-        if "name" in request.POST:
+        if request.POST.get("action") == "back":
+            # Go back to the previous step
+            if step == 'feature':
+                # Go back to greeting (name input)
+                request.session['step'] = 'greeting'
+                context['greeting'] = None
+                context['feature'] = None
+            elif step == 'recommender':
+                # Go back to feature selection
+                request.session['step'] = 'feature'
+                context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
+                context['feature'] = None
+            elif step == 'greeting':
+                # Already at the first step, maybe flush session
+                request.session.flush()
+                context = {
+                    "greeting": None,
+                    "feature": None,
+                    "movie_result": None,
+                }
+            # Add more elifs for other features if needed
+
+        elif "name" in request.POST:
             name = request.POST.get("name", "").capitalize()
             if name:
                 greeter = GreetingPrompt(name)
                 greeting = greeter.generate_prompt()
                 request.session['name'] = name
-                request.session['step'] = 'choose_feature'
+                request.session['step'] = 'feature'
                 context['greeting'] = greeting
 
         elif "feature" in request.POST:
             feature = request.POST.get("feature")
-            if feature == 'back':
-                request.session.flush()
-                context = {}
-            elif feature == 'recommender':
+            if feature == 'recommender':
                 request.session['step'] = 'recommender'
                 context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
                 context['feature'] = 'recommender'
-                context['show_recommender_prompt'] = True
             else:
                 request.session['step'] = feature
                 context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
                 context['feature'] = feature
-
-        elif "action" in request.POST:
-            if request.POST.get("action") == "back":
-                request.session['step'] = 'choose_feature'
-                context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
-                context['feature'] = None
-                context['movie_result'] = None
 
         elif "movie_prompt" in request.POST:
             user_input = request.POST.get("movie_prompt")
@@ -50,7 +65,25 @@ def greet_view(request):
             context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
 
     else:
-        request.session.flush()
+        # On GET, start at greeting
+        request.session['step'] = 'greeting'
+        context = {
+            "greeting": None,
+            "feature": None,
+            "movie_result": None,
+        }
+
+    # Set context for template based on step
+    step = request.session.get('step', 'greeting')
+    if step == 'greeting':
+        context['greeting'] = None
+        context['feature'] = None
+    elif step == 'feature':
+        context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
+        context['feature'] = None
+    elif step == 'recommender':
+        context['greeting'] = GreetingPrompt(request.session.get('name')).generate_prompt()
+        context['feature'] = 'recommender'
 
     return render(request, "movie_rec/recommend.html", context)
 
