caf950031ce7bcf3f6b77d190b20e96e8c1ff673
BrendxnW <BrendonHorse18@gmail.com>
2026-04-26 16:27:38 -0700

    added registration saving

--- movie_rec/views.py
+++ movie_rec/views.py
@@ -2,8 +2,10 @@ import os
 import pandas as pd
 
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from .nlp_utils import RecommendMovie, Remixer, ClassifyIntent, FindMovie
 from .tmdb_API import get_movies_by_genre, InvalidGenreError, get_movie_plot
+from .forms import CustomUserCreationForm
 
 
 _movie_titles = None
@@ -27,19 +29,23 @@ def load_imdb_titles():
 
 
 def login(request):
-
-
     return render(request, "login.html")
 
 
 def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
 
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = CustomUserCreationForm()
 
-    return render(request, "register.html")
+    return render(request, "register.html", {"form": form})
 
 
 def home(request):
-
     return render(request,"home.html")
 
 def recommender(request):
